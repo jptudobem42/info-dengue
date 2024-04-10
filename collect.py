0714c3f5d323f59df1d89ee7a0ed58229621f5bf
joao.paulo@credencial.net.br <joao.paulo@credencial.net.br>
2024-04-10 22:37:08 +0000

    cleanup: remoção de funções desnecessárias

--- collect.py
+++ collect.py
@@ -1,10 +1,9 @@
 # Databricks notebook source
 from datetime import datetime
 import time
-import pandas as pd
-import requests
 import json
-from pyspark.sql import SparkSession 
+from pyspark.sql import SparkSession
+import requests
 
 # COMMAND ----------
 
@@ -17,48 +16,39 @@ class Collector:
         query = "SELECT cod_municipio FROM codigos_ibge_municipios"
         df = self.spark.sql(query)
         return [row.cod_municipio for row in df.collect()]
-
-    def get_content(self, **kwargs):
-        resp = requests.get(self.url, params=kwargs)
-        return resp
     
     def save_json(self, data, disease, cod_municipio, year, week):
         file_path = f"/dbfs/mnt/datalake/info-dengue/raw/{disease}/{cod_municipio}-{year}{week}.json"
-        with open(file_path, "w") as open_file:
-            json.dump(data, open_file)
-    
-    def save_data(self, data, disease, cod_municipio, year, week):
-        self.save_json(data, disease, cod_municipio, year, week)
+        with open(file_path, "w") as f:
+            json.dump(data, f)
 
-    def get_and_save(self, disease, cod_municipio, year, week, **kwargs):
+    def get_and_save(self, disease, cod_municipio, year, week):
         params = {
-            'geocode': cod_municipio,
-            'disease': disease,
-            'format': 'json',
-            'ew_start': str(week),
-            'ew_end': str(week),
-            'ey_start': str(year),
-            'ey_end': str(year),
-            **kwargs
+            "geocode": cod_municipio,
+            "disease": disease,
+            "format": "json",
+            "ew_start": str(week),
+            "ew_end": str(week),
+            "ey_start": str(year),
+            "ey_end": str(year),
         }
-        resp = self.get_content(**params)
-        if resp.status_code == 200:
+        response = requests.get(self.url, params=params)
+
+        if response.status_code == 200:
             try:
-                data = resp.json()
-                self.save_data(data, disease, cod_municipio, year, week)
+                data = response.json()
+                self.save_json(data, disease, cod_municipio, year, week)
                 return data
             except json.JSONDecodeError:
-                print(f"Erro ao decodificar JSON da resposta para {disease}, {cod_municipio}, {year}, semana {week}. Conteúdo: {resp.text}")
+                print(f"Erro ao decodificar JSON: {disease}, {cod_municipio}, {year}, semana {week}")
                 return None
         else:
-            print(f"Request sem sucesso: {resp.status_code}. Conteúdo: {resp.text}")
+            print(f"Request sem sucesso: {response.status_code}")
             return None
 
     def get_current_week(self):
-        today = datetime.now()
-        year, week, day_of_week = today.isocalendar()
-        return week
-    
+        return datetime.now().isocalendar().week
+
     def collect_data(self, diseases, year_start, year_end):
         municipios = self.get_municipios()
         for disease in diseases:
@@ -67,18 +57,7 @@ class Collector:
                 for week in range(1, last_week + 1):
                     for cod_municipio in municipios:
                         print(f"Coletando {disease} para {cod_municipio} no ano {year}, semana {week}...")
-                        data = self.get_and_save(
-                                                disease=disease, 
-                                                cod_municipio=cod_municipio, 
-                                                year=year, 
-                                                week=week,
-                                                geocode=cod_municipio, 
-                                                format='json', 
-                                                ew_start=str(week), 
-                                                ew_end=str(week), 
-                                                ey_start=str(year), 
-                                                ey_end=str(year)
-                                                )
+                        data = self.get_and_save(disease, cod_municipio, year, week)
                         if data is not None:
                             print(f"Coleta concluída para {disease} em {cod_municipio}, ano {year}, semana {week}.")
                         time.sleep(30)
